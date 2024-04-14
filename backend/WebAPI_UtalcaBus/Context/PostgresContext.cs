74450e5dd917efd33a111784335b0c51e11840d6
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2024-04-13 22:34:55 -0400

    feat(backend): dbcontext por dbidentitycontext

--- backend/WebAPI_UtalcaBus/Context/PostgresContext.cs
+++ backend/WebAPI_UtalcaBus/Context/PostgresContext.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using WebAPI_UtalcaBus.Entities;
 
 namespace WebAPI_UtalcaBus.Context;
 
-public class PostgresContext: DbContext
+public class PostgresContext: IdentityDbContext
 {
     public PostgresContext(DbContextOptions<PostgresContext> options): base(options)
     {
