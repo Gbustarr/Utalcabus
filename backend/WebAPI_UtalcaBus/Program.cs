3337eabde389d828ebbda87cb8bf0d0c2dab67d0
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2024-04-13 23:16:23 -0400

    feat(backend): creador de roles de app + creador de administrador [Se usa NETCORE.IDENTITY]

--- backend/WebAPI_UtalcaBus/Program.cs
+++ backend/WebAPI_UtalcaBus/Program.cs
@@ -1,11 +1,10 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 using WebAPI_UtalcaBus.Config;
 using WebAPI_UtalcaBus.Context;
+using DotNetEnv;
 
-
+Env.Load(); 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("Connection");
@@ -34,7 +33,9 @@ var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
+    
     var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
 
     // Crear roles si no existen
     foreach (var roleName in new[] { RolesConfig.AdministradorRole, RolesConfig.ConductorRole, RolesConfig.PasajeroRole })
@@ -43,6 +44,41 @@ using (var scope = app.Services.CreateScope())
         {
             await roleManager.CreateAsync(new IdentityRole(roleName));
         }
+    }
+    
+    var usuariosAdministradores = await userManager.GetUsersInRoleAsync("Administrador");
+    ConsoleColor originalColor = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Red;
+
+    if (usuariosAdministradores.Count == 0)
+    {
+        System.Console.WriteLine("PROGRAM.CS => No existe un usuario administrador");
+        var defaultAdmin = "administrador@utalcabus.cl";
+        var contrasena = builder.Configuration["CONTRASENA_ADMIN"];
+   
+        if (!string.IsNullOrEmpty(contrasena))
+        {
+            var existeAdmin = await userManager.FindByNameAsync(defaultAdmin);
+            if (existeAdmin == null)
+            {
+                var nuevoUsuarioAdmin = new IdentityUser { UserName = defaultAdmin };
+                var resultado = await userManager.CreateAsync(nuevoUsuarioAdmin, contrasena);
+                if (resultado.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(nuevoUsuarioAdmin, "Administrador");
+                    Console.WriteLine("PROGRAM.CS => administrador@utalcabus.cl creado");
+                }
+                else
+                {
+                    Console.WriteLine("PROGRAM.CS => Error en la contraseña");
+                }
+            }
+        }
+    }
+    else
+    {
+        
+        Console.Write("PROGRAM.CS => Ya existe un usuario administrador");
     }
 }
 
