99f75be9e0dd8a70961d1a060d2101a127a903f3
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2024-04-13 22:35:51 -0400

    feat(backend): roles por defecto + rutas identity (incluye registrarse y login, sin asociar usuario a un rol aún)

--- backend/WebAPI_UtalcaBus/Program.cs
+++ backend/WebAPI_UtalcaBus/Program.cs
@@ -1,16 +1,51 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using WebAPI_UtalcaBus.Config;
 using WebAPI_UtalcaBus.Context;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("Connection");
 builder.Services.AddDbContext<PostgresContext>(options => options.UseNpgsql(connectionString));
 
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.SignIn.RequireConfirmedEmail = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 0;
+});
+
+builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<PostgresContext>().AddApiEndpoints()
+    .AddDefaultTokenProviders();
+
 var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    // Crear roles si no existen
+    foreach (var roleName in new[] { RolesConfig.AdministradorRole, RolesConfig.ConductorRole, RolesConfig.PasajeroRole })
+    {
+        if (!await roleManager.RoleExistsAsync(roleName))
+        {
+            await roleManager.CreateAsync(new IdentityRole(roleName));
+        }
+    }
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -18,6 +53,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGroup("/identity").MapIdentityApi<IdentityUser>();
 
 app.UseHttpsRedirection();
 app.MapControllers();
